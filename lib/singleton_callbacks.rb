37395364f808faa023205bd66a787edbc1910934
Cezar Almeida <cezar.almeidajr@gmail.com>
2016-04-27 23:40:46 -0300

    SingletonCallbacks and SingletonCallbacksSpec improvements

--- lib/singleton_callbacks.rb
+++ lib/singleton_callbacks.rb
@@ -27,14 +27,10 @@ module SingletonCallbacks extend ActiveSupport::Concern
 
         def add_to_callback_list(callback_type, method_sym, callback_sym, options)
           @callbacks[callback_type] << {
-              method_sym: method_sym,
-              callback_sym: callback_sym,
-              options: options
+            method_sym: method_sym,
+            callback_sym: callback_sym,
+            options: options
           }
-        end
-
-        def eval_as_singleton
-          self.singleton_class.class_eval &Proc.new if block_given?
         end
 
         def replace_original_method(method_sym)
@@ -43,7 +39,7 @@ module SingletonCallbacks extend ActiveSupport::Concern
 
           save_original_method method_sym
           create_substitute_method method_sym, substitute_method_name
-          eval_as_singleton { alias_method method_sym, substitute_method_name }
+          class_eval { alias_method method_sym, substitute_method_name }
         end
 
         def substitute_method_name_for(method_sym)
@@ -52,13 +48,13 @@ module SingletonCallbacks extend ActiveSupport::Concern
 
         def substitute_method_already_exists?(substitute_method_name)
           already_exists = true
-          eval_as_singleton { already_exists = method_defined? substitute_method_name }
+          class_eval { already_exists = method_defined? substitute_method_name }
           already_exists
         end
 
         def save_original_method(method_sym)
           original_method_sym = original_method_name_for(method_sym).to_sym
-          eval_as_singleton { alias_method original_method_sym, method_sym }
+          class_eval { alias_method original_method_sym, method_sym }
         end
 
         def original_method_name_for(method_sym)
@@ -66,23 +62,32 @@ module SingletonCallbacks extend ActiveSupport::Concern
         end
 
         def create_substitute_method(method_sym, substitute_method_name)
-          eval_as_singleton do
+          class_eval do
             define_method substitute_method_name do
               execute_callbacks_for :before, method_sym
-              r = send original_method_name_for(method_sym)
+              r = send(eval_as_singleton { original_method_name_for(method_sym) })
               execute_callbacks_for :after, method_sym
               r
             end
           end
         end
 
-        def execute_callbacks_for(callback_type, method_sym)
-          @callbacks[callback_type].select{ |c| c[:method_sym].eql? method_sym }.each do |callback|
-            send callback[:callback_sym]
-          end
-        end
-
       end
+
+      def execute_callbacks_for(callback_type, method_sym)
+        callbacks = get_callbacks_for(callback_type, method_sym)
+        callbacks.each { |c| send c[:callback_sym] }
+      end
+
+      def get_callbacks_for(callback_type, method_sym)
+        callbacks = self.singleton_class.instance_variable_get(:@callbacks)
+        callbacks[callback_type].select{ |c| c[:method_sym].eql? method_sym }
+      end
+
+      def eval_as_singleton
+        self.singleton_class.class_eval &Proc.new if block_given?
+      end
+
     end
   end
 
